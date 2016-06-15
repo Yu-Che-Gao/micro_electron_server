50ec81a64eaba82abc9cb81634d7509be3b202b3
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-06-16 01:36:56 +0800

    重新release

--- electron-quick-start-linux-ia32/resources/app/message_box.js
+++ electron-quick-start-linux-ia32/resources/app/message_box.js
@@ -1,8 +1,8 @@
 const electron = require('electron');
-const ipc=electron.ipcMain;
-const dialog=electron.dialog;
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
 
-exports.setInformationDialog=function(myTitle, myMessage, myButtons) {
+exports.setInformationDialog = function (myTitle, myMessage, myButtons, handler) {
     ipc.on('open-information-dialog', function (event) {
         const options = {
             type: 'info',
@@ -12,10 +12,7 @@ exports.setInformationDialog=function(myTitle, myMessage, myButtons) {
         };
         dialog.showMessageBox(options, function (index) {
             event.sender.send('information-dialog-selection', index);
-
-            if(index===0) {
-               dialog.showErrorBox('提醒', '你點了是');
-            }
+            handler(index);
         });
     });
 };
--- electron-quick-start-win32-ia32/resources/app/window_event.js
+++ electron-quick-start-win32-ia32/resources/app/window_event.js
@@ -1,7 +1,6 @@
-const electron=require('electron');
-
-exports.createWindow=function() {
-  let mainWindow = new electron.BrowserWindow({width: 800, height: 600});
+const electron = require('electron');
+exports.createWindow = function () {
+  let mainWindow = new electron.BrowserWindow({ width: 800, height: 600, frame: false });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.on('closed', function () {
     mainWindow = null;
